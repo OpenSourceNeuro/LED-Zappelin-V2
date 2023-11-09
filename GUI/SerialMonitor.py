44a84708243f07206ad3db4a639c38cbcb689241
Maxime Zimmermann <35870697+MaxZimmer@users.noreply.github.com>
2023-11-09 14:24:36 +0100

    delete branch

--- GUI/SerialMonitor.py
+++
@@ -1,6 +0,0 @@
-
-
-class SerialError():
-
-    def LEDZap_LED_NotConnected(self):
-        self.ui.LED_Zap_Serial_label.setText('LED Zappelin is not connected: LED value change will not be applied')
