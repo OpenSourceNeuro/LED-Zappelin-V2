790b3291bdfbf38a4bcfff8280b0b590e0660f2b
Maxime Zimmermann <35870697+MaxZimmer@users.noreply.github.com>
2024-01-23 02:24:27 +0100

    upload Attach-Interrupt version

---
+++ Arduino/LED_Zappelin_V2_ATTR-DirectTrigger/LED_Zappelin_V2_ATTR-DirectTrigger.ino
@@ -0,0 +1,64 @@
+#include "Serial_functions.h"
+
+/* ----------------------------------------------------------------------------------*/
+/* --------------------------- Settings and Initialisings ---------------------------*/
+
+void setup() {
+  HardwareSettings();             // Load Hardware Settings as described in "General_Settings.h"
+  SetWavelength();                // Set the LEDs wavelength as described in "LED_Values.h"  
+  SetNeoPixelColours();           // Light up LED in sequence when the stimulator is reset
+  SCmdAddCommand();               // Initiate Serial Command actions
+}
+
+
+/* ----------------------------------------------------------------------------------*/
+/* ----------------------------------- Main Loop ------------------------------------*/
+  
+void loop() {
+  SCmd.readSerial();                                 // Wait for Serial Command
+  
+  CurrentMicros = micros();                          // Pick up current running time in microseconds
+  DiffMicros = CurrentMicros - PreviousMicros;       // Calculate 
+  
+  if (PreAdaptationFlag == false){
+    if (DiffMicros >= ResolutionMicros) {
+      PreviousMicros = micros();
+
+      if (StimulusFlag == true){
+        Serial.println(i);
+        i += 1;
+        if(i >= iLoop){
+          i = 1;
+        }
+      }
+    }
+  }
+  
+  if (PreAdaptationFlag == true){
+    if (DiffMicros >= PreAdaptMicros) {
+      PreviousMicros = micros();
+      PreAdaptationFlag = false;
+
+      if (StimulusFlag == true){
+        Serial.println(i);
+        i += 1;
+      }
+    }
+  }
+
+  if (TriggerFlag == true){
+    TriggerFlag = false;
+    TriggerState = digitalRead(Trigger);
+    if (TriggerState == HIGH){
+      LED_On();
+    }
+    else {
+      LED_Off();
+    }
+  }
+
+}
+
+
+
+
